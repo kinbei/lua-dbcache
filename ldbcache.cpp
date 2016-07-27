2c7a785c67de2198b55292f0c728b599549406c9
kinbei <kinbei@qq.com>
2016-07-27 22:29:15 +0800

    Hello World

--- ldbcache.cpp
+++ ldbcache.cpp
@@ -20,20 +20,20 @@ linit(lua_State *L) {
 	const char * memdbname = lua_tostring(L, 1);
 	luaL_argcheck(L, memdbname != NULL, 1, "memdbname can't be null");
 	size_t initsize = lua_tointeger(L, 2);
-	if (!initsize) {
-		initsize = 100;
+	if (initsize) {
+		initsize = 1024;
 	}
 	size_t incsize = lua_tointeger(L, 3);
-	if (!incsize) {
+	if (incsize) {
 		incsize = 20;
 	}
 	size_t idxsize = lua_tointeger(L, 4);
-	if (!idxsize) {
-		idxsize = 100;
+	if (idxsize) {
+		idxsize = 1024;
 	}
 	size_t maxsize = lua_tointeger(L, 5);
-		if (!maxsize) {
-		maxsize = 200;
+		if (maxsize) {
+		maxsize = 2048;
 	}
 
 	cleanupsem( memdbname );
@@ -96,87 +96,173 @@ lclose(lua_State *L) {
 	return 0;
 }
 
+static int
+lgettable(lua_State *L) {
+	const char *tablename = lua_tostring(L, 1);
+
+	lua_pushstring(L, tablename);
+	lua_gettable(L, lua_upvalueindex(1));
+	lua_rawget(L, -1);
+
+	return 1;
+}
+
 //////////////////////////////////////////////////////////////////////////////////////////////////
 /* table record define */
 struct tb_activity_record {
 	cli_int8_t activity_id;
-	char activity_name[100];
+	char *activity_name;
+	cli_int8_t status;
 };
 
 /* table field define */
 struct cli_field_descriptor tb_activity_descriptor[] = {
 		{ cli_int8, cli_indexed, "activity_id", },
 		{ cli_asciiz, 0, "activity_name" },
+		{ cli_int8, cli_indexed, "status", },		
 };
 
+/*
 static int
-ltest(lua_State *L) {
-	int session = lua_tointeger(L, 1);
-
-	int r = cli_create_table(session, "tb_activity", sizeof(tb_activity_descriptor)/sizeof(cli_field_descriptor), tb_activity_descriptor);
-	if ( r != cli_ok ) {		
-		return luaL_error(L, "Failed to insert(%d)", r);
+create_table(lua_State *L, int session) {
+	int rc = cli_create_table(session, "tb_activity", sizeof(tb_activity_descriptor)/sizeof(cli_field_descriptor), tb_activity_descriptor);
+	if ( rc != cli_ok ) {		
+		return luaL_error(L, "Failed to create table with code(%d)", rc);
 	}
-	
-	struct tb_activity_record t;
-	memset(&t, 0x00, sizeof(t));
-	t.activity_id = 0xFFFFFFFFFFFFFFFF;
-	strcpy(t.activity_name, "this is a activity name");
+	return 0;
+}
+*/
 
-	int statement = cli_statement(session, "insert into tb_activity");
+/* save temp value of per record */
+tb_activity_record _tb_activity_record;
+
+/////////////////////////////////////////////////////////////////////////
+struct dbrecordset {
+	tb_activity_record _tb_activity_record;
+};
+static struct dbrecordset g_dbrecord;
+
+struct dbstatement {
+	int _tb_activity_statement;
+};
+static struct dbstatement g_statement;
+
+static int 
+tb_activity_reset(lua_State *L) {
+	memset(&g_dbrecord._tb_activity_record, 0x00, sizeof(g_dbrecord._tb_activity_record));
+	return 0;
+}
+
+static int
+tb_activity_setactivity_id(lua_State *L) {
+	lua_Integer v = lua_tointeger(L, 1);
+	g_dbrecord._tb_activity_record.activity_id = v;
+	return 0;
+}
+
+static int
+tb_activity_setactivity_name(lua_State *L) {
+	size_t len = 0;
+	const char *v = lua_tolstring(L, 1, &len);
+	if (len < sizeof(g_dbrecord._tb_activity_record.activity_name)-1 ) {
+		len = sizeof(g_dbrecord._tb_activity_record.activity_name)-1;
+	}
+	memcpy(g_dbrecord._tb_activity_record.activity_name, v, len);
+	return 0;
+}
+
+static int
+tb_activity_setstatus(lua_State *L) {
+	lua_Integer v = lua_tointeger(L, 1);
+	g_dbrecord._tb_activity_record.status = v;
+	return 0;
+}
+
+static int
+tb_activity_insert(lua_State *L) {
+	int session = lua_tointeger(L, 1);
+	int statement = cli_statement(session, "insert info tb_activity");
 	if ( statement < 0 ) {
 		return luaL_error(L, "Failed to create statement with code(%d)", statement);
 	}
 
-	if( ( r = cli_column(statement, "activity_id", cli_int8, NULL, &t.activity_id ) != cli_ok ) ||
-		( r = cli_column(statement, "activity_name", cli_asciiz, NULL, t.activity_name ) != cli_ok ) ) {
-		return luaL_error(L, "Failed to call cli_column with code(%d)", r);
+	int rc;
+	if( ( rc = cli_column(statement, "activity_id", cli_int8, NULL, &g_dbrecord._tb_activity_record.activity_id ) != cli_ok ) ||
+		( rc = cli_column(statement, "activity_name", cli_asciiz, NULL, g_dbrecord._tb_activity_record.activity_name ) != cli_ok ) ||
+		( rc = cli_column(statement, "status", cli_int8, NULL, &g_dbrecord._tb_activity_record.status ) != cli_ok )
+		) {
+		return luaL_error(L, "Failed to call cli_column with code(%d)", rc);
 	}
 
 	cli_oid_t oid;
-	r = cli_insert(statement, &oid);
-	if ( r != cli_ok ) {
-		return luaL_error(L, "Failed to insert with code(%d)", r);
+	rc = cli_insert(statement, &oid);
+	if ( rc != cli_ok ) {
+		return luaL_error(L, "Failed to insert with code(%d)", rc);
 	}
 
-	r = cli_free(statement);
-	if ( r != cli_ok ) {
-		return luaL_error(L, "Failed to free statement with code(%d)", r);
-	}
-
-	statement = cli_statement(session, 
-		"select * from tb_activity where activity_id = %activity_id");
-	if ( statement < 0 ) {
-		return luaL_error(L, "Failed to prepare query with code(%d)", statement);
-	}
-
-	struct tb_activity_record rt;
-	memset(&rt, 0x00, sizeof(rt));
-	
-	if( ( r = cli_column(statement, "activity_id", cli_int8, NULL, &rt.activity_id ) != cli_ok ) ||
-		( r = cli_column(statement, "activity_name", cli_asciiz, NULL, rt.activity_name ) != cli_ok ) ) {
-		return luaL_error(L, "Failed to call cli_column with code(%d)", r);
-	}
-
-	cli_int8_t n = 0xFFFFFFFFFFFFFFFF;
-	if ((r = cli_parameter(statement, "%activity_id", cli_int8, &n)) != cli_ok )
-    {
-        fprintf(stderr, "cli_parameter failed with code %d\n", r);
-        return EXIT_FAILURE;
-    }
-
-	r = cli_fetch(statement, cli_view_only);
-	if (r < 0 ) { 
-        fprintf(stderr, "cli_fetch 1 returns %d instead of 1\n", r);
-        return EXIT_FAILURE;
-    }
-
-	while ((r = cli_get_next(statement)) == cli_ok) { 	
-		printf("Record activity_id(0x%llX) activity_name(%s) \n", rt.activity_id, rt.activity_name);
+	rc = cli_free(statement);
+	if ( rc != cli_ok ) {
+		return luaL_error(L, "Failed to free statement with code(%d)", rc);
 	}
 
 	return 0;
 }
+
+static int
+tb_activity_prepare(lua_State *L) {
+	const char *subsql = lua_tostring(L, 2);
+	int session = lua_tointeger(L, 1);
+
+	char sql[255] = {0};
+	snprintf(sql, sizeof(sql), "select * from tb_activity where %s", subsql);
+	g_statement._tb_activity_statement = cli_statement(session, sql);
+	if ( g_statement._tb_activity_statement < 0 ) {
+		return luaL_error(L, "Failed to prepare query with code(%d)", g_statement._tb_activity_statement);
+	}
+
+	return 0;
+}
+
+/*
+static int
+tb_activity_setactivity_id(lua_State *L) {
+	int rc = cli_column(g_statement._tb_activity_statement, "activity_id", cli_int8, NULL, &g_dbrecord._tb_activity_record.activity_id);
+	if ( rc != cli_ok ) {
+		return luaL_error(L, "Failed to cli_column with code(%d)", rc);
+	}
+	return 0;
+}
+*/
+
+/*
+static int
+tb_activity_setactivity_name(lua_State *L) {
+	int rc = cli_column(g_statement._tb_activity_statement, "activity_name", cli_asciiz, NULL, g_dbrecord._tb_activity_record.activity_name);
+	if ( rc != cli_ok ) {
+		return luaL_error(L, "Failed to cli_column with code(%d)", rc);
+	}
+	return 0;
+}
+*/
+
+/*
+static int
+tb_activity_setstatus(lua_State *L) {
+	int rc = cli_column(g_statement._tb_activity_statement, "status", cli_int8, NULL, g_dbrecord._tb_activity_record.status);
+	if ( rc != cli_ok ) {
+		return luaL_error(L, "Failed to cli_column with code(%d)", rc);
+	}
+	return 0;
+}*/
+
+#define DECLARE_FUNCTION(tablename, funcname) \
+do \
+{ \
+	lua_pushcfunction(L, tablename##_##funcname ); \
+	lua_pushliteral(L, #funcname); \
+	lua_rawset(L, -2); \
+} \
+while (0); \
 
 extern "C" int
 luaopen_dbcache_core(lua_State *L) {
@@ -189,10 +275,27 @@ luaopen_dbcache_core(lua_State *L) {
 		{ "commit", lcommit },
 		{ "rollback", lrollback },
 		{ "close", lclose },
-		{ "test", ltest },
+		{ "gettable", lgettable },
 		{ NULL, NULL },
 	};
 
 	luaL_newlib(L, l);
+
+	lua_newtable(L); // upvalue
+
+	// tb_activity
+	lua_newtable(L);
+	DECLARE_FUNCTION(tb_activity, reset);
+	DECLARE_FUNCTION(tb_activity, setactivity_id);
+	DECLARE_FUNCTION(tb_activity, setactivity_name);
+	DECLARE_FUNCTION(tb_activity, setstatus);
+	DECLARE_FUNCTION(tb_activity, insert);
+	DECLARE_FUNCTION(tb_activity, prepare);
+	lua_pushliteral(L, "tb_activity");
+	lua_rawset(L, -2);
+
+	// sharing previous table as upvalue
+	luaL_setfuncs(L, l, 1);
+
 	return 1;
 }
--- test.lua
+++ test.lua
@@ -1,6 +1,64 @@
 local dbcache = require "dbcache"
-dbcache.init "test"
+
+local function dbopt()
+	-- insert testcase
+	local tb_activity = dbcache.gettable("tb_activity")
+	tb_activity.reset()
+	tb_activity.setactivity_id(100)
+	tb_activity.setactivity_name("test_act_name")
+	tb_activity.setstatus(1)
+	tb_activity.insert()
+
+	-- select testcase
+	tb_activity.reset()
+	tb_activity.prepare("activity_id = %activity_id and status = %status")
+	tb_activity.setactivity_id(100)
+	tb_activity.setstatus(1)
+	local row = tb_activity.find()
+	assert(row == 1)
+	while tb_activity.next() then
+		assert( tb_activity.getactivity_id() == 100 )
+		assert( tb_activity.getactivity_name() == "test_act_name" )
+		assert( tb_activity.getstatus() == 1 )
+
+		tb_activity.setactivity_name("test_act_name_1")
+		tb_activity.update()
+	end
+
+	-- update testcase
+	tb_activity.reset()
+	tb_activity.prepare("activity_id = %activity_id and status = %status")
+	tb_activity.setactivity_id(100)
+	tb_activity.setstatus(1)
+	local row = tb_activity.find()
+	assert(row == 1)
+	while tb_activity.next() then
+		assert( tb_activity.getactivity_id() == 100 )
+		assert( tb_activity.getactivity_name() == "test_act_name_1" )
+		assert( tb_activity.getstatus() == 1 )
+	end
+
+	-- remove testcase
+	tb_activity.reset()
+	tb_activity.prepare("")
+	tb_activity.find()
+	while tb_activity.next() then
+		if tb_activity.getactivity_id() == 100 then
+			tb_activity.remove()
+		end
+	end
+	tb_activity.reset()
+	tb_activity.prepare("")
+	assert( tb_activity.find() == 0 )
+end
+
+dbcache.opendb("test")
 dbcache.begin()
-dbcache.test()
-dbcache.rollback()
-dbcache.commit()
+local ok, err = pcall(dbopt)
+if not ok then
+	print(err)
+	dbcache.rollback()
+else
+	dbcache.commit()
+end
+dbcache.closedb()
